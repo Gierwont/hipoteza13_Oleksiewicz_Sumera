26e3ba6f01343a6dc2ce9037249615734d66a6b8
student <student@ckziu.jaworzno.pl>
2022-12-07 10:18:31 +0100

    Trójkowy_alfa

--- index.py
+++ index.py
@@ -15,4 +15,22 @@ def binary(x,k):
         if(y>=1):
             y = y-1 ;
     return wynik
-print(binary(x,k))
+
+def trójkowy(x,k):
+    wynik = "0,"
+    y = x
+    for i in range(k):
+        if(y>=(2/3)):
+            wynik += "2"
+        elif(y>=(1/3)):
+            wynik += "1"
+        elif (y>=0):
+            wynik += "0"
+        y = y * 3
+        if(y>=2):
+            y = y-1
+        if(y>=1):
+            y = y-2
+    return wynik
+
+print(trójkowy(x,k))
